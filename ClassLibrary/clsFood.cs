b103606e156b17af5b5764bc4503472816ef15a2
Yaseen Fulat <yas_0dst@hotmail.co.uk>
2019-03-08 14:01:33 +0000

    New Classes

---
+++ ClassLibrary/clsFood.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsFood
+    {
+        // private data members
+        private int mFoodID;
+        private string mFoodName;
+        private string mFoodType;
+        private decimal mSellPrice;
+
+        // public properties
+        public int FoodID
+        {
+            get
+            {
+                return mFoodID;
+            }
+            set
+            {
+                mFoodID = value;
+            }
+        }
+
+        public string FoodName
+        {
+            get
+            {
+                return mFoodName;
+            }
+            set
+            {
+                mFoodName = value;
+            }
+        }
+
+        public string FoodType
+        {
+            get
+            {
+                return mFoodType;
+            }
+            set
+            {
+                mFoodType = value;
+            }
+        }
+
+        public decimal SellPrice
+        {
+            get
+            {
+                return mSellPrice;
+            }
+            set
+            {
+                mSellPrice = value;
+            }
+        }
+
+        // find method
+        public bool Find(int FoodID)
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@FoodID", FoodID);
+            DB.Execute("sproc_tblFood_FilterByFoodID");
+            if (DB.Count == 1)
+            {
+                // copy data
+                mFoodID = Convert.ToInt32(DB.DataTable.Rows[0]["FoodID"]);
+                mFoodName = Convert.ToString(DB.DataTable.Rows[0]["FoodName"]);
+                mFoodType = Convert.ToString(DB.DataTable.Rows[0]["FoodType"]);
+                mSellPrice = Convert.ToDecimal(DB.DataTable.Rows[0]["SellPrice"]);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ ClassLibrary/clsFoodCollection.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class clsFoodCollection
+    {
+        // private data member for the food list items
+        private List<clsFood> mFoods = new List<clsFood>();
+        // private data member ThisFood
+        clsFood mThisFood = new clsFood();
+
+        // public property ThisFood
+        public clsFood ThisFood
+        {
+            get
+            {
+                return mThisFood;
+            }
+            set
+            {
+                mThisFood = value;
+            }
+        }
+
+        // public class constructor
+        public clsFoodCollection()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblFood_SelectAll");
+            // get the count
+            Int32 RecordCount = DB.Count;
+            // set index
+            Int32 Index = 0;
+            // while loop
+            while (Index < RecordCount)
+            {
+                // create class instance
+                clsFood Food = new clsFood();
+                // get data
+                Food.FoodID = Convert.ToInt32(DB.DataTable.Rows[Index]["FoodID"]);
+                Food.FoodName = Convert.ToString(DB.DataTable.Rows[Index]["FoodName"]);
+                Food.FoodType = Convert.ToString(DB.DataTable.Rows[Index]["FoodType"]);
+                Food.SellPrice = Convert.ToDecimal(DB.DataTable.Rows[Index]["SellPrice"]);
+                // add record
+                mFoods.Add(Food);
+                // increment index
+                Index++;
+            }
+        }
+
+        // public property for all foods
+        public List<clsFood> FoodList
+        {
+            get
+            {
+                return mFoods;
+            }
+            set
+            {
+                mFoods = value;
+            }
+        }
+
+        // public count property
+        public int Count
+        {
+            get
+            {
+                return mFoods.Count;
+            }
+            set
+            {
+                // later
+            }
+        }
+
+        // public add method
+        public int Add()
+        {
+            clsDataConnection DB = new clsDataConnection();
+            DB.AddParameter("@FoodName", mThisFood.FoodName);
+            DB.AddParameter("@FoodType", mThisFood.FoodType);
+            DB.AddParameter("@SellPrice", mThisFood.SellPrice);
+            return DB.Execute("sproc_tblFood_Insert");
+        }
+
+        
+    }
+}
